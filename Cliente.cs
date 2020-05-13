75d832664635d45d2bffc76cd24cc9da7b62d5b5
isabelsantana <isabelsantana@ucl.br>
2020-05-13 23:55:02 +0000

    Adição de atributos na classe Cliente

--- Cliente.cs
+++ Cliente.cs
@@ -1,7 +1,44 @@
 class Cliente{
 
-  public double cnpj;
-  public string nome;
-  public double codigo;
+  private string endereco;
+  private string telefone;
+  private string nome;
+  private int codigo;
+
+  public string Endereco{
+    get{
+      return endereco;
+    }
+    set{
+      endereco = value;
+    }
+  }
+
+  public string Telefone{
+    get{
+      return telefone;
+    }
+    set{
+      telefone = value;
+    }
+  }
+
+  public string Nome{
+    get{
+      return nome;
+    }
+    set{
+      nome = value;
+    }
+  }
+
+  public int Codigo{
+    get{
+      return codigo;
+    }
+    set {
+      codigo = value;
+    }
+  }
   
 }
