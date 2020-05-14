b9571f9bd9bed48f9e611c53b658b94a37e54e1c
isabelsantana <isabelsantana@ucl.br>
2020-05-14 00:50:55 +0000

    .

--- CriacaoChamado.cs
+++ CriacaoChamado.cs
@@ -1,1 +1,3 @@
-class CriacaoChamado{}
+class CriacaoChamado{
+  
+}
--- main.cs
+++ main.cs
@@ -1,26 +1,39 @@
 using System;
+using System.IO;
 
 class MainClass {
   public static void Main (string[] args) {
 
     Cliente c = new Cliente();
+    string opcao;
 
-    Console.WriteLine("Informe o nome do cliente: ");
-    c.Nome = Console.ReadLine();
-    //Console.WriteLine(c.Nome); 
-    Console.WriteLine();
+    Console.WriteLine("**SISTEMA DE SUPORTE DE TI**");
+    Console.WriteLine("Olá, digite 1 para cadastrar um novo cliente e 2 caso só queira ver a lista de clientes!");
+    opcao = Console.ReadLine();
 
-    Console.WriteLine("Informe o telefone: ");
-    c.Telefone = Console.ReadLine();
-    Console.WriteLine();
+    if (opcao == "1"){
+    
 
-    Console.WriteLine("Informe o endereço do cliente: ");
-    c.Endereco = Console.ReadLine();
-    Console.WriteLine();
+      Console.WriteLine("Informe o nome do cliente: ");
+      c.Nome = Console.ReadLine();
+      //Console.WriteLine(c.Nome); 
+      Console.WriteLine();
 
-    Console.WriteLine("Informe o código do cliente: ");
-    c.Codigo = int.Parse(Console.ReadLine());
-    Console.WriteLine();
+      Console.WriteLine("Informe o telefone: ");
+      c.Telefone = Console.ReadLine();
+      Console.WriteLine();
+
+      Console.WriteLine("Informe o endereço do cliente: ");
+      c.Endereco = Console.ReadLine();
+      Console.WriteLine();
+
+      Console.WriteLine("Informe o código do cliente: ");
+      c.Codigo = int.Parse(Console.ReadLine());
+      Console.WriteLine();
+      
+      File.WriteAllText("cadastroCliente.txt", "**CADASTRO DO CLIENTE**\nNome: " +c.Nome + "\nTelefone: " +c.Telefone + "\nEndereco: "+c.Endereco +"\nCódigo: " +c.Codigo);
+
+    }    
 
     
   }
