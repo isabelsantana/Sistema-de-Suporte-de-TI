795ef98ab50d1cea04d0149b49d3f857d98865e5
isabelsantana <isabelsantana@ucl.br>
2020-05-14 00:06:27 +0000

    cadastro clientes atualizado

--- main.cs
+++ main.cs
@@ -2,6 +2,26 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
+
+    Cliente c = new Cliente();
+
+    Console.WriteLine("Informe o nome do cliente: ");
+    c.Nome = Console.ReadLine();
+    //Console.WriteLine(c.Nome); 
+    Console.WriteLine();
+
+    Console.WriteLine("Informe o telefone: ");
+    c.Telefone = Console.ReadLine();
+    Console.WriteLine();
+
+    Console.WriteLine("Informe o endereço do cliente: ");
+    c.Endereco = Console.ReadLine();
+    Console.WriteLine();
+
+    Console.WriteLine("Informe o código do cliente: ");
+    c.Codigo = int.Parse(Console.ReadLine());
+    Console.WriteLine();
+
     
   }
 }
