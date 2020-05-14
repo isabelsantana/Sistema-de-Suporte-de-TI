9b264cc695848a6dc86e66184df443b347a0cd02
isabelsantana <isabelsantana@ucl.br>
2020-05-14 06:29:09 +0000

    adição atributos da classe CadastroDispositivo

--- CadastroDispositivo.cs
+++ CadastroDispositivo.cs
@@ -1,3 +1,37 @@
-class CadastroDispósitivo{
+class CadastroDispositivo{
+
+  private string memoria;
+  private string hd;
+  private string ssd;
+  private string placaDeVideo;
+  private string codigoDoComputador;
+
+
+  public string Memoria{
+    get{
+      return memoria;
+    }
+    set{
+      memoria = value;
+    }
+  }
   
+  public string Hd{
+    get{
+      return hd;
+    }
+    set{
+      hd = value;
+    }
+  }
+
+  public string Ssd{
+    get{
+      return ssd;
+    }
+    set{
+      ssd = value;
+    }
+  }
+
 }
